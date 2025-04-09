e9a6aa7bffb425e6506bd6db3652acc239e06f6a
Mohammad Al-Abbasi <mohammad.al-abbasi@student.tugraz.at>
2025-04-09 12:03:06 +0200

    add phone number

--- Models/Entities/Customer.cs
+++ Models/Entities/Customer.cs
@@ -13,7 +13,7 @@ namespace MyApiProject.Models.Entities
                         
         [BsonElement("_email"), BsonRepresentation(MongoDB.Bson.BsonType.String)]
         public string? Email { get; set; }
-        // public string PhoneNumber { get; set; }
+        public string PhoneNumber { get; set; } = string.Empty;
         // public DateTime CreatedAt { get; set; }
         // public DateTime UpdatedAt { get; set; }
 
